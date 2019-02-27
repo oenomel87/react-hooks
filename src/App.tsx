533eac17dd38748092adb6be1b9f8f3671678cad
yspark <ysparknanum@gmail.com>
2019-02-27 15:18:55 +0900

    state hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,54 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+interface ITodo {
+  title: string,
+  datetime: Date,
+}
+
+const Todo = (todo: ITodo) => {
+  return (
+    <div className="todo">
+      <div className="title">{ todo.title }</div>
+    </div>
+  );
+}
+
+const App = () => {
+
+  const [list, setList] = useState<Array<ITodo>>([]);
+  const addTodo = () => {
+    setList(list.concat([{
+      title: getTitle(),
+      datetime: new Date()
+    }]));
+    resetTitle();
   }
+
+  const getTitle = (): string => {
+    const elem = document.querySelectorAll('input.title')[0] as HTMLInputElement;
+    return elem.value;
+  }
+
+  const resetTitle = (): void => {
+    const elem = document.querySelectorAll('input.title')[0] as HTMLInputElement;
+    elem.value = ''; 
+  }
+
+  return (
+    <div className="app">
+      <h1 className="hero">My toodo</h1>
+      <div className="input-wrap">
+        <div className="input">
+          <input type="text" className="title"/>
+        </div>
+        <button onClick={e => addTodo()}>Add</button>
+      </div>
+      <div className="list">
+        { list.map((todo: ITodo, index: number) => <Todo key={index} title={todo.title} datetime={todo.datetime}/>) }
+      </div>
+    </div>
+  );
 }
 
 export default App;
